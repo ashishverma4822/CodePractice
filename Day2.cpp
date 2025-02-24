cd918e1feb371a33717e7b6bc8511081d896a4a7
Ashish Kumar Verma <35832574+ashishverma4822@users.noreply.github.com>
2025-02-24 19:39:56 +0530

    Update Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -1,3 +1,4 @@
+// Problem 1 : Remove Half Nodes
 class Solution {
   private:
     Node* hiAyu(Node* root){
@@ -13,5 +14,40 @@ class Solution {
         // code here
         return hiAyu(root);
         
+    }
+};
+
+// Problem 2 : Balanced Binary Tree
+class Solution {
+private:
+    int height(TreeNode* root){
+        if(root==nullptr) return 0;
+        int l = 1 + height(root->left);
+        int r = 1 + height(root->right);
+        return max(l,r); 
+    }
+    bool hiAyu(TreeNode* root){
+        if(!root) return true; 
+        int l = height(root->left);
+        int r = height(root->right);
+        bool hoKiNahi = abs(l-r) > 1 ? false : true;
+        bool ls = hiAyu(root->left);
+        bool rs = hiAyu(root->right);
+        return hoKiNahi and ls and rs;
+    }
+    int hiAyu2(TreeNode* root){
+        if(!root) return 0;
+        int l = 1+hiAyu2(root->left);
+        int r = 1+hiAyu2(root->right);
+        haiKiNahi = haiKiNahi and (abs(l-r)>1 ? false : true);
+        return max(l,r);
+    }
+public:
+    bool haiKiNahi = true;
+    bool isBalanced(TreeNode* root) {
+        // return hiAyu(root);
+        haiKiNahi = true;
+        hiAyu2(root);
+        return haiKiNahi;
     }
 };
