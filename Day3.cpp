d94c83a57e36287f02537ca9eb75ec247921e5fd
Ashish Kumar Verma <35832574+ashishverma4822@users.noreply.github.com>
2025-02-28 11:48:00 +0530

    Update Day3.cpp

--- Day3.cpp
+++ Day3.cpp
@@ -18,3 +18,23 @@ public:
         return uttar;
     }
 };
+
+// Problem 2 : Count Complete Tree Node
+
+
+//Problem 3 : Minimum Depth of Bnary Tree
+class Solution {
+private:    
+    int hiAyu(TreeNode* root){
+        if(!root) return 0;
+        if(!root->left and !root->right) return 1;
+        int l = 1e5, r = 1e5;
+        if(root->left) l = hiAyu(root->left);
+        if(root->right) r = hiAyu(root->right);
+        return 1+min(l,r);
+    }
+public:
+    int minDepth(TreeNode* root) {
+        return hiAyu(root);
+    }
+};
