84b51282b8d370c8d017f1072ff94b84ae496cb4
Ashish Kumar Verma <35832574+ashishverma4822@users.noreply.github.com>
2025-03-03 18:39:57 +0530

    Update Day8.cpp

--- Day8.cpp
+++ Day8.cpp
@@ -1,5 +1,5 @@
 
-//Problem : 572. Subtree of Another Tree
+//Problem 1 : 572. Subtree of Another Tree
 
 class Solution {
 private:  
@@ -18,5 +18,48 @@ private:
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         return hiAyu(root,subRoot);
+    }
+};
+
+// Problem 2 : Isomorphic Trees
+
+class Solution {
+  private:
+    bool hiAyu(Node* root1, Node* root2){
+        if(!root1 and !root2) return true;
+        if(!root1 and root2) return false;
+        if(root1 and !root2) return false;
+        if(root1->data!=root2->data) return false;
+        bool f1 = hiAyu(root1->left,root2->right) and hiAyu(root1->right,root2->left);
+        bool f2 = hiAyu(root1->left,root2->left) and hiAyu(root1->right,root2->right);
+        return f1 or f2;
+    }
+  public:
+    // Return True if the given trees are isomotphic. Else return False.
+    bool isIsomorphic(Node *root1, Node *root2) {
+        // add code here
+        return hiAyu(root1,root2);
+    }
+};
+
+// Problem 3 : 872. Leaf-Similar Trees
+
+class Solution {
+private:
+    void hiAyu(TreeNode* root, vector<int>& v){
+        if(!root) return;
+        if(!root->left and !root->right){
+            v.push_back(root->val);
+            return;
+        }
+        hiAyu(root->left,v);
+        hiAyu(root->right,v);
+    }
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> r1,r2;
+        hiAyu(root1,r1);
+        hiAyu(root2,r2);
+        return r1==r2;
     }
 };
