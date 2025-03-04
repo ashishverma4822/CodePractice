6cf4ded9feecb54625221331a1c5863638a191b7
Ashish Kumar Verma <35832574+ashishverma4822@users.noreply.github.com>
2025-03-04 14:47:14 +0530

    Update Day9.cpp

--- Day9.cpp
+++ Day9.cpp
@@ -28,3 +28,35 @@ public:
         return hiAyu(root);
     }
 };
+
+
+// Problem 2 :107. Binary Tree Level Order Traversal II
+
+class Solution {
+private:
+    vector<vector<int>> hiAyu(TreeNode* root){
+        if(!root) return {};
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<vector<int>> uttar;
+        vector<int> v;
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                auto f = q.front();
+                q.pop();
+                if(f) v.push_back(f->val);
+                if(f->left) q.push(f->left);
+                if(f->right) q.push(f->right);
+            }
+            uttar.push_back(v);
+            v.clear();
+        }
+        reverse(uttar.begin(),uttar.end());
+        return uttar;
+    }
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        return hiAyu(root);
+    }
+};
